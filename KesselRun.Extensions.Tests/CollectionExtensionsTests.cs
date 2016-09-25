8768e11d3da602ea0f01235036a1da434b7d23a3
David Rogers <davidrogersau@yahoo.com.au>
2016-09-25 16:07:37 +0930

    Added an ExceptionExtensions class and and AddOrUpdate method to extend IDictionary<T,V>.

---
+++ KesselRun.Extensions.Tests/CollectionExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace KesselRun.Extensions.Tests
+{
+    [TestClass]
+    public class CollectionExtensionsTests
+    {
+        [TestMethod]
+        public void AddOrUpdateAddsItemWhereNoneExisted()
+        {
+            //  Arrange
+            var testDictionary = GetTestDictionary();
+            //  Act
+            testDictionary.AddOrUpdate("Item3", "I got added");
+
+            //  Assert                        
+            Assert.AreEqual(testDictionary["Item3"], "I got added");
+        }
+
+        [TestMethod]
+        public void AddOrUpdateAddsItemWhereItemExisted()
+        {
+            //  Arrange
+            var testDictionary = GetTestDictionary();
+
+            //  Act
+            testDictionary.AddOrUpdate("Item2", "I got added");
+
+            //  Assert                        
+            Assert.AreEqual(testDictionary["Item2"], "I got added");
+        }
+
+        private Dictionary<string, string> GetTestDictionary()
+        {
+            Dictionary<string, string> testDictionary = new Dictionary<string, string>
+            {
+                {"Item1", "Value2"},
+                {"Item2", "Value2"}
+            };
+            return testDictionary;
+        }
+    }
+}
+
---
+++ KesselRun.Extensions.Tests/ExceptionExtensionsTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KesselRun.Extensions.Tests
+{
+    [TestClass]
+    public class ExceptionExtensionsTests
+    {
+        [TestMethod]
+        public void MessagesAsDictionaryIncludesMessageOfInnerException()
+        {
+            Dictionary<Exception, string> aggregate = new Dictionary<Exception, string>(2);
+
+            try
+            {
+                ThrowException();
+            }
+            catch (Exception exception)
+            {
+                exception.MessagesAsDictionary(aggregate);
+                Assert.AreEqual(aggregate.First().Value, "This is the top level exception");
+            }
+        }
+
+        [TestMethod]
+        public void MessagesAsDictionaryIncludesMessageOfInnerExceptionAlt()
+        {
+            Dictionary<Exception, string> aggregate = new Dictionary<Exception, string>(2);
+
+            try
+            {
+                ThrowException();
+            }
+            catch (Exception exception)
+            {
+                try
+                {
+                    WrapException(exception);
+                }
+                catch (Exception newException)
+                {
+                    newException.MessagesAsDictionary(aggregate);
+                    Assert.AreEqual(3, aggregate.Count);
+                }
+            }
+        }
+
+        public void ThrowException()
+        {
+            throw new Exception("This is the top level exception",new Exception("This is the inner excpetion"));
+        }
+
+        public void WrapException(Exception exception)
+        {
+            throw new Exception("This is the now the top level exception", exception);
+        }
+    }
+}
